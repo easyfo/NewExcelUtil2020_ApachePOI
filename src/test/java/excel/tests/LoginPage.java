1c24d9d80fd1c9544b2bb3042a21cedf7080faa9
easyfo <88473945+easyfo@users.noreply.github.com>
2021-08-16 18:41:37 +0530

    Update LoginPage.java

--- src/test/java/excel/tests/LoginPage.java
+++ src/test/java/excel/tests/LoginPage.java
@@ -13,7 +13,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginPage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main method
 
 		
 		WebDriverManager.chromedriver().setup();
